6fe98164572113534cba85269102b98cb1927b5f
Ntagungira-cmd <ntagungiraali@gmail.com>
2021-11-13 20:16:20 +0200

    Adding the search functionality

--- pages/events/search.js
+++ pages/events/search.js
@@ -1,11 +1,12 @@
+import qs from 'qs';
 import Layout from "@/components/Layout";
 import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 
 export default function SearchPage({ events }) {
   return (
-    <Layout>
-      <h1>Events</h1>
+    <Layout title="search results">
+      <h1>Search results</h1>
       {events.length === 0 ? (
         <h3>No events to by the name</h3>
       ) : (
@@ -15,7 +16,17 @@ export default function SearchPage({ events }) {
   );
 }
 export async function getServerSideProps({ query: { term } }) {
-  const res = await fetch(`${API_URL}/events?name_contains=date:${term}`);
+  const query=qs.stringify({
+    _where:{
+      _or:[
+        {name_contains:term},
+        {venue_contains:term},
+        {perfomers_contains:term},
+        {description_contains:term},
+      ]
+    }
+  })
+  const res = await fetch(`${API_URL}/events?${query}`);
   const events = await res.json();
   //console.log(events);
   return {
