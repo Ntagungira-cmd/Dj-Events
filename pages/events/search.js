7d7b12cde1844a461204729e68c20b660b4b2b95
Ntagungira-cmd <ntagungiraali@gmail.com>
2021-11-15 21:38:57 +0200

    Adding the search component

--- pages/events/search.js
+++ pages/events/search.js
@@ -1,14 +1,16 @@
 import qs from "qs";
+import Link from "next/link"
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
 
 export default function SearchPage({ events }) {
-  const router=useRouter();
+  const router = useRouter();
   //console.log(router);
   return (
     <Layout title="search results">
+      <Link href="/events">Go Back</Link>
       <h1>Search results for {router.query.term}</h1>
       {events.length === 0 ? (
         <h3>No events to by the name</h3>
